cc5086506b6446eee33ce2f0cce29b37e4b666d5
Adnan Hossain <48421257+ah168105@users.noreply.github.com>
2022-07-27 18:38:24 -0500

    Create ASCII Business Card.py

---
+++ ASCII Business Card.py
@@ -0,0 +1,17 @@
+#### ---- SETUP ---- ####
+
+#### ---- INPUT ---- ####
+print("Enter your information below to get a custom business card")
+first_name = input ("Your First Name: ")
+last_name = input ("Your Last Name: ")
+dream_job = input ("Your Dream Job: ")
+
+#### ---- OUTPUT ---- ####
+print("")
+print("Now generating your custom business card..... ")
+print("")
+print("-----@-----")
+print(first_name)
+print(last_name)
+print(dream_job)
+print("-----@-----")
