ff46c994783de413f32025f0220b47a39d9e1475
Adnan Hossain <48421257+ah168105@users.noreply.github.com>
2022-07-27 18:59:00 -0500

    Create Favorite Things.py

---
+++ Favorite Things.py
@@ -0,0 +1,12 @@
+#### ---- INPUT ---- ####
+food = input("What is your favorite food? ")
+free_time = input("What is your favorite thing to do during free time? ")
+subject = input("What is your favorite subject? ")
+print("")
+print("")
+#### ---- OUTPUT ---- ####
+print("Here is a list of your favorite things!")
+print("")
+print(food)
+print(free_time)
+print(subject)
