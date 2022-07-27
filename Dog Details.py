df66b4306279a1ed71d659560af6bd62e775ed21
Adnan Hossain <48421257+ah168105@users.noreply.github.com>
2022-07-27 18:57:12 -0500

    Create Dog Details.py

---
+++ Dog Details.py
@@ -0,0 +1,22 @@
+#### ---- IMAGE OUTPUT ---- ####
+print("Dog Name: Milo")
+#Head begins
+print("    ___  ")
+print(" __/ / \ ")
+print("|    |/\ ")
+print("|_--\   \              /^==~ ")
+#Body begins
+print("     \   \-___________/ /  ")
+print("      \                : ")
+print("      |                : ")
+print("      |       ___ \    ) ")
+print("       \|  __/     \  )  ")
+#Legs begins
+print("        | /         \  \ ")
+print("        | }          ( ( ")
+print("        ||           \ | ")
+print("        ||            || ")
+print("       / )           / ) ")
+print("       --/           --/  ")
+print("Favorite Food: Scooby-Doo Snacks")
+print("Favorite Toy: Rubber Bone Toy")
