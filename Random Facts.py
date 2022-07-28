9c2c803d402af766cf3630c0ee0513ae2cd3654b
Adnan Hossain <48421257+ah168105@users.noreply.github.com>
2022-07-27 19:52:25 -0500

    Create Random Facts.py

---
+++ Random Facts.py
@@ -0,0 +1,17 @@
+# Get a random number 
+import random
+choice = random.randint(1,3)
+
+# Print a Programming fun fact when the randint is 1
+if choice == 1:
+    print("The first programmmer was Ada Lovelace, born in 1815")
+
+
+# Make a Nature fun fact when the randint is 2
+if choice == 2:
+    print("Pineapples take 2 years to grow")
+
+
+# Make a miscellaneous fun fact when the randint is 3
+if choice == 3:
+    print("Peanuts aren't technically nuts.")
