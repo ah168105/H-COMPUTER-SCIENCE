7f7013ad139bf7bec714418e547c60dd19672ce5
Adnan Hossain <48421257+ah168105@users.noreply.github.com>
2022-07-27 19:46:19 -0500

    Create Problem-Solver.py

---
+++ Problem-Solver.py
@@ -0,0 +1,39 @@
+#### ---- QUESTIONS ---- ####
+
+## -- Problem question -- ##
+response = ("Write your response here: ")
+print("What is a problem in your school that matters to you? ")
+problem = input(response)
+print("")
+print("")
+print("---------------------")
+
+## -- Solution question -- ##
+print("How do you think technology could be used to solve this problem? ")
+technology = input(response)
+print("")
+print("")
+print("---------------------")
+## -- Skills question -- ##
+print("What new skills will you need to learn in order to create this technology? ")
+new_skill = input(response)
+#### ---- SUMMARY ---- ####
+print("")
+print("")
+print("---------------------")
+## -- Introduction -- ##
+print("What is today's date?")
+date = input(response)
+print("Here is a copy of your answers and the day you wrote them. Write them down so you can come back later and see how close you are to achieving your goals! ")
+print("")
+
+
+
+## -- Answers -- ##
+print(problem)
+
+print(technology)
+
+print(new_skill)
+
+print(date)
