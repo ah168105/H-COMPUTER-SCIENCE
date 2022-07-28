f941f6b38c9e770e392467ddd4e029968b9ef164
Adnan Hossain <48421257+ah168105@users.noreply.github.com>
2022-07-27 19:03:16 -0500

    Create Fishtank Runner.py

---
+++ Fishtank Runner.py
@@ -0,0 +1,15 @@
+#### ---- STRING VARIABLES ---- ####
+
+top = "*"
+bubble_2 = "  o   "
+bubble_1 = "   o  "
+fish = "><>   "
+bottom = "_"
+
+#### ---- RUNNER DESIGN ---- ####
+print(top * 36)
+print("")
+print(bubble_2 * 6)
+print(bubble_1 * 6)
+print(fish * 6)
+print(bottom * 36)
