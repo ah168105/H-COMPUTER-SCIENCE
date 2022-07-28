ea5a52587cb92d3e3e9c110c352d5c6c12f56be9
Adnan Hossain <48421257+ah168105@users.noreply.github.com>
2022-07-27 19:54:54 -0500

    Create Reducing Waste (Water Bottles).py

---
+++ Reducing Waste (Water Bottles).py
@@ -0,0 +1,12 @@
+#### ---- WATER BOTTLE INPUT ---- ####
+bottles = int(input("How many bottles of water do you drink per day? "))
+
+
+
+#### ---- CALCULATION ---- ####
+bottles = str(bottles * 365)
+
+
+#### ---- YEARLY WASTE OUTPUT ---- ####
+print("By using a reusable bottle, you can keep " + bottles + " disposable bottles out of the landfill each year." )
+
